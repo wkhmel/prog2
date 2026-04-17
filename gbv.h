468c88b941190ce00ef9c88851acd2655f4756e0
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-17 14:25:01 -0300

    Declarei gbv_find

--- gbv.h
+++ gbv.h
@@ -23,6 +23,7 @@ typedef struct {
 // Funções que voce deve implementar em gbv.c
 int gbv_create(const char *filename);
 int gbv_open(Library *lib, const char *filename);
+int gbv_find(Library *lib, const char *procurado);
 int gbv_add(Library *lib, const char *archive, const char *docname);
 int gbv_remove(Library *lib, const char *docname);
 int gbv_list(const Library *lib);
