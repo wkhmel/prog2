28acb9a7728b866f0473c30a1775867eba8dc3f2
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-17 11:13:01 -0300

    incluí parâmetro novo no gbv_view

--- gbv.h
+++ gbv.h
@@ -26,7 +26,7 @@ int gbv_open(Library *lib, const char *filename);
 int gbv_add(Library *lib, const char *archive, const char *docname);
 int gbv_remove(Library *lib, const char *docname);
 int gbv_list(const Library *lib);
-int gbv_view(const Library *lib, const char *docname);
+int gbv_view(const Library *lib, const char *archive, const char *docname);
 int gbv_order(Library *lib, const char *archive, const char *criteria);
 
 #endif
